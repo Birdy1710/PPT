71ec17fbb2354303950f97f3f8eb6f00a479b3d2
nthanhloc <154595456+nthanhloc@users.noreply.github.com>
2023-12-23 05:19:56 +0700

    Update centralDifferenceDerivative (final).m

--- centralDifferenceDerivative (final).m
+++ centralDifferenceDerivative (final).m
@@ -1,4 +1,4 @@
-function approximate_derivative = centralDifferenceDerivative(func, evaluation_point, h, order_of_accuracy)
+function derivative = centralDifferenceDerivative(func, evaluation_point, h, order_of_accuracy)
     % Kiểm tra order_of_accuracy có phải là 1 hoặc 2 không
     if order_of_accuracy ~= 1 && order_of_accuracy ~= 2
         error('Chỉ hỗ trợ giá trị sai số O(h) hoặc O(h^2).');
@@ -6,9 +6,9 @@ function approximate_derivative = centralDifferenceDerivative(func, evaluation_p
     
     % Tính đạo hàm bằng phương pháp chuỗi Taylor chiều trung tâm
     if order_of_accuracy == 1
-        approximate_derivative = (func(evaluation_point + h) - func(evaluation_point - h)) / (2 * h);
+        derivative = (func(evaluation_point + h) - func(evaluation_point - h)) / (2 * h);
     else
-        approximate_derivative = (func(evaluation_point - 2*h) - 8*func(evaluation_point - h) + 8*func(evaluation_point + h) - func(evaluation_point + 2*h)) / (12 * h);
+        derivative = (func(evaluation_point - 2*h) - 8*func(evaluation_point - h) + 8*func(evaluation_point + h) - func(evaluation_point + 2*h)) / (12 * h);
     end
     
 end
