4f9b650d0986f0c1622419a65acf755df52826a9
nthanhloc <154595456+nthanhloc@users.noreply.github.com>
2023-12-23 05:17:29 +0700

    Update backwardDifferenceDerivative (final).m

--- backwardDifferenceDerivative (final).m
+++ backwardDifferenceDerivative (final).m
@@ -1,4 +1,4 @@
-function approximate_derivative = backwardDifferenceDerivative(func, evaluation_point, h, order_of_accuracy)
+function derivative = backwardDifferenceDerivative(func, evaluation_point, h, order_of_accuracy)
     % Kiểm tra order_of_accuracy có phải là 1 hoặc 2 không
     if order_of_accuracy ~= 1 && order_of_accuracy ~= 2
         error('Chỉ hỗ trợ giá trị sai số O(h) hoặc O(h^2).');
@@ -6,9 +6,9 @@ function approximate_derivative = backwardDifferenceDerivative(func, evaluation_
     
     % Tính đạo hàm bằng phương pháp chuỗi Taylor chiều lùi
     if order_of_accuracy == 1
-        approximate_derivative = (func(evaluation_point) - func(evaluation_point - h)) / h;
+        derivative = (func(evaluation_point) - func(evaluation_point - h)) / h;
     else
-        approximate_derivative = (func(evaluation_point - 2*h) - 4*func(evaluation_point - h) + 3*func(evaluation_point)) / (2 * h);
+        derivative = (func(evaluation_point - 2*h) - 4*func(evaluation_point - h) + 3*func(evaluation_point)) / (2 * h);
     end
     
 end
