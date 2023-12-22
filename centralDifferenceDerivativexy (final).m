88c17d193376d5e69d36ba3ddc323075a5ed9c2c
nthanhloc <154595456+nthanhloc@users.noreply.github.com>
2023-12-23 02:40:05 +0700

    Bản fix cuối cùng

---
+++ centralDifferenceDerivativexy (final).m
@@ -0,0 +1,27 @@
+function derivative_at_evaluation_point = centralDifferenceDerivativexy(x_values, f_values, evaluation_point, order_of_accuracy)
+    % Kiểm tra độ dài của chuỗi đầu vào
+    if length(x_values) ~= length(f_values)
+        error('Độ dài của chuỗi x và chuỗi f phải giống nhau.');
+    end
+    
+    % Kiểm tra order_of_accuracy có phải là 1 hoặc 2 không
+    if order_of_accuracy ~= 1 && order_of_accuracy ~= 2
+        error('Chỉ hỗ trợ giá trị sai số O(h) hoặc O(h^2).');
+    end
+    
+    % Tính đạo hàm bằng phương pháp chuỗi Taylor chiều trung tâm
+    h = x_values(2) - x_values(1); % Giả sử khoảng cách giữa các điểm x là h (đơn giản để tính toán)
+    
+    % Tính đạo hàm bằng O(h) hoặc O(h^2) tùy thuộc vào order_of_accuracy
+    if order_of_accuracy == 1
+        derivative = (f_values(2:end) - f_values(1:end-1)) / (2 * h);
+    else
+        derivative = (f_values(3:end) - 2*f_values(2:end-1) + f_values(1:end-2)) / (2 * h^2);
+    end
+    
+    % Tính giá trị đạo hàm tại điểm đánh giá
+    evaluation_index = find(x_values == evaluation_point);
+    derivative_at_evaluation_point = derivative(evaluation_index - 1);
+    
+    fprintf('Đạo hàm chiều trung tâm tại điểm đánh giá %.2f là: %.4f\n', evaluation_point, derivative_at_evaluation_point);
+end
